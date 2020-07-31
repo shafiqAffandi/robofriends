c63c931e8791dcc1d839dec926dfa6c547cd6d54
shafiqAffandi <66380507+shafiqAffandi@users.noreply.github.com>
2020-07-31 21:52:54 +0700

    change robots data from static to dynamic

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import CardList from './CardList';
-import { robots } from './robots.js';
-import SearchBox from './SearchBox.js';
+import Scroll from './Scroll'
+import SearchBox from './SearchBox';
 import './App.css'
 
 class App extends Component {
   constructor() {
     super()
     this.state = {
-      robots: robots,
+      robots: [],
       searchfield : ''
     }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => {
+      return response.json();
+    })
+    .then(users => {
+      this.setState({robots: users});
+    })
   }
 
   onSearchChange = (event) =>  {
@@ -21,13 +31,19 @@ class App extends Component {
     const filterRobot = this.state.robots.filter(robots => {
       return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     })
-    return(
-      <div className='tc'>
-        <h1 className='f1'>RoboFriends</h1>
-        <SearchBox searchChange={this.onSearchChange}/>
-        <CardList robots={filterRobot} />
-      </div>
-    );
+    if(this.state.robots.length === 0){
+      return <h1 className='tc'>Loading...</h1>
+    }else{
+      return(
+        <div className='tc'>
+          <h1 className='f1'>RoboFriends</h1>
+          <SearchBox searchChange={this.onSearchChange}/>
+          <Scroll>
+            <CardList robots={filterRobot} />
+          </Scroll>
+        </div>
+      );
+    }
   }
 }
 
