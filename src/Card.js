6c62fe54aaa82ecde3b29e19192b36dbe63f9bb3
shafiqAffandi <66380507+shafiqAffandi@users.noreply.github.com>
2020-07-31 19:32:31 +0700

    first commit

---
+++ src/Card.js
@@ -0,0 +1,17 @@
+import React, { Fragment } from 'react';
+
+const Card = (props) => {
+  const { name, email, username } = props;
+  return (
+    <div className='tc bg-light-green dib br3 pa3 ma3 grow bw2 shadow-5'>
+      <img alt='robot' src={`https://robohash.org/${name + '12'}?200x200`} />
+      <Fragment>
+        <h2>{name}</h2>
+        <h3>{username}</h3>
+        <p>{email}</p>
+      </Fragment>
+    </div>
+  );
+}
+
+export default Card;
