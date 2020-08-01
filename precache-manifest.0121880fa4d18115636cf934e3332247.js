290da234b91c96d263b003ceec36a131e530158d
shafiqAffandi <66380507+shafiqAffandi@users.noreply.github.com>
2020-08-01 12:11:32 +0700

    Updates

---
+++ precache-manifest.0121880fa4d18115636cf934e3332247.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1683186881e2d3768081b324a4eebf3f",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e4ce5be8baa82c260e46",
+    "url": "/robofriends/static/css/main.0ffac5f0.chunk.css"
+  },
+  {
+    "revision": "005ad378bc2817b28083",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/robofriends/static/js/2.f690e5d6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4ce5be8baa82c260e46",
+    "url": "/robofriends/static/js/main.9f36acd0.chunk.js"
+  },
+  {
+    "revision": "e7d2cbb368e0813da61d",
+    "url": "/robofriends/static/js/runtime-main.74eee7b5.js"
+  },
+  {
+    "revision": "add573b7dfa735ab4e4ed24dd94a40ee",
+    "url": "/robofriends/static/media/Robot Reavers.add573b7.woff"
+  }
+]);
